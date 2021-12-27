2f59297a7c1db5932d0fbb14b8af93642657dac6
zhaoshuliang <809894780@qq.com>
2021-12-27 14:06:20 +0800

    爬虫xpath 和数据库连接

--- chapter15.py
+++ chapter15.py
@@ -7,9 +7,17 @@ connect = pymysql.connect(host='127.0.0.1',
                 db='chapter01',
                 charset='utf8')
 cur = connect.cursor()
+#sql 查询
 sql = 'select * from sys_user'
 cur.execute(sql)
 data = cur.fetchall()
 for i in data:
     print(i)
+insert_sql = """insert into sys_user(username,password) values('李四','2033')"""
+#sql插入
+try:
+    cur.execute(insert_sql)
+    connect.commit()
+except:
+    connect.rollback()
 cur.close()
---
+++ chapter16.py
@@ -0,0 +1,21 @@
+import requests
+from lxml import etree
+
+# 爬取58租房信息
+headers = {
+    'User-Agent' :'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36'
+    }
+url = "https://bj.58.com/chuzu/pn{0}/?PGTID=0d3090a7-0000-1840-3c31-74c281b4647c&ClickID=3"
+# 58租房url
+for page in range(1,5):
+    page = str(page)
+    new_url = url.format(page)
+    page_text = requests.get(url=url,headers=headers).text
+    tree = etree.HTML(page_text)
+    li_list = tree.xpath('//ul[@class="house-list"]/li')
+    for li in li_list:
+        house_detail = li.xpath('./div[@class="des"]//text()')
+        house_detail = ''.join(house_detail).replace("\n",' ').replace(" ",'')
+        house_price = li.xpath('./div[@class="list-li-right"]//b[@class="strongbox"]/text()')
+        house_price = ''.join(house_price)
+        print(house_detail + "========>" + house_price)
